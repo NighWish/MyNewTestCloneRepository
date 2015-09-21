df399c035aef7980dc5ef3a9193ef912760f5f67
NightWish <1598004992@qq.com>
2015-09-21 18:18:27 +0800

    create project

---
+++ MyFirstRemoteGit/MyFirstRemoteGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstRemoteGit
+//
+//  Created by jt on 15/9/21.
+//  Copyright © 2015年 com.jtjr.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirstRemoteGit/MyFirstRemoteGitTests/MyFirstRemoteGitTests.m
@@ -0,0 +1,39 @@
+//
+//  MyFirstRemoteGitTests.m
+//  MyFirstRemoteGitTests
+//
+//  Created by jt on 15/9/21.
+//  Copyright © 2015年 com.jtjr.www. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyFirstRemoteGitTests : XCTestCase
+
+@end
+
+@implementation MyFirstRemoteGitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
